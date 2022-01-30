8cfe80da7673fe8802cd58a2e32f04528b0fd5ff
Andron <lyashenko.andron@mail.ru>
2022-01-30 10:34:56 +0300

    try to import QT

--- 2 (2).py
+++ 2 (2).py
@@ -2,18 +2,22 @@ import os
 import random
 import sys
 import pygame
+import csv
 pygame.font.init()
 
 
 FIRES_ARRAY = []
+SKINS = ['dgj_blue_3.png']
 SKIN = 'WHITE'
 ENEMIES_ARRAY = []
 SIZE = 1000, 1000
 # SIZE = 800, 800
 CNT = 0
-LIFES = 3
+LIFES = 5
 TYPE_OF_LEVEL = [(0, 0, 20), (40, 10, 30)]
 COLOR_INDEX = 0
+SHTANI = None
+USERNAME = ''
 
 
 def load_image(name, per_pixel_alpha=False, color_key=None):
@@ -45,10 +49,6 @@ class Board:
 
     def render(self):
         self.screen.fill('yellow')
-        for i in range(50):
-            self.screen.fill(pygame.Color('black'),
-                        (random.random() * self.width,
-                         random.random() * self.height, 4, 4))
 
 
 class BoardShop:
@@ -73,6 +73,7 @@ class Fire(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
 
+
 class Hero_for_menu(pygame.sprite.Sprite):
     def __init__(self, group):
         super().__init__(group)
@@ -85,7 +86,10 @@ class Hero_for_menu(pygame.sprite.Sprite):
 class Hero(pygame.sprite.Sprite):
     def __init__(self, group):
         super().__init__(group)
-        self.image = load_image("big_dgj.png", True)
+        if SHTANI is None:
+            self.image = load_image("big_dgj.png", True)
+        else:
+            self.image = load_image(SHTANI, True)
         self.rect = self.image.get_rect()
         self.rect.x = SIZE[0] // 2
         self.rect.y = SIZE[1] // 2
@@ -126,14 +130,20 @@ def game_over():
 
 
 def tick(hero, to_menu_screen, stars_screen):
-    global ENEMIES_ARRAY, CNT
+    global ENEMIES_ARRAY, CNT, LIFES
     shipes_colides = pygame.sprite.Group([ship for ship in ENEMIES_ARRAY])
     for i in FIRES_ARRAY:
         i.move()
+    terminate = False
     for ship in ENEMIES_ARRAY:
         ship.move()
-        if len(pygame.sprite.spritecollide(hero, shipes_colides, False)):
-            start_screen(to_menu_screen)
+        if ship.rect.x > 1000:
+            terminate = True
+        if terminate:
+            LIFES -= 1
+            terminate = False
+            ENEMIES_ARRAY.remove(ship)
+
     for fire in FIRES_ARRAY:
         tmp = pygame.sprite.spritecollide(fire, shipes_colides, False)
         if len(tmp):
@@ -158,11 +168,19 @@ def level_color():
 def score(screen):
     f1 = pygame.font.Font(None, 50)
     text1 = f1.render(f'SCORE: {CNT}', True,
-                      (255, 255, 255))
-    screen.blit(text1, (800, 100))
+                      (0, 0, 0))
+    text2 = f1.render(f'LIFES: {LIFES}', True,
+                      (0, 0, 0))
+    screen.blit(text1, (800, 900))
+    screen.blit(text2, (800, 950))
+
+
+def enter_name():
+    pass
 
 
 def start_screen(screen):
+    global LIFES, CNT
     all_sprites = pygame.sprite.Group()
     hero = Hero_for_menu(all_sprites)
     screen2 = pygame.Surface(SIZE)
@@ -171,6 +189,8 @@ def start_screen(screen):
     hero.rect.y = SIZE[0] // 6
     fon = pygame.transform.scale(load_image('fon.jpg'), SIZE)
     screen.blit(fon, (0, 0))
+    LIFES = 5
+    CNT = 0
 
     while True:
         hero.rect.x += cnt
@@ -197,13 +217,13 @@ def start_screen(screen):
             cnt = 1
         if hero.rect.y <= 50:
             cnt2 = 1
-        screen2.fill((0, 0, 0))
         screen.blit(fon, (0, 0))
         all_sprites.draw(screen)
         pygame.display.flip()
 
 
 def main(n):
+    global SHTANI
     size = SIZE
     menu_screen = pygame.display.set_mode(size)
     if n == 1:
@@ -224,7 +244,16 @@ def main(n):
         running = True
         while running:
             if LIFES == 0:
-                game_over()
+                access = False
+
+                start_screen(menu_screen)
+                # with open('table.csv', 'w', newline='') as f:
+                #     writer = csv.DictWriter(
+                #         f, fieldnames=list(data[0].keys()),
+                #         delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+                #     writer.writeheader()
+                #     for d in data:
+                #         writer.writerow(d)
             k += 1
             if k % 300 == 0:
                 spawn_enemy(board, enemy_sprites)
@@ -243,7 +272,7 @@ def main(n):
                     running = False
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
-                        shoot(fire_sprites, hero.rect.left + hero.rect.width // 2 - 20, hero.rect.top + hero.rect.height // 2 - 20)
+                        shoot(fire_sprites, hero.rect.left + hero.rect.width // 2 - 90, hero.rect.top + hero.rect.height // 2 - 50)
                 if hero.rect.top < 600:
                     hero.rect.top = 600
                 if hero.rect.left < 0:
@@ -255,7 +284,7 @@ def main(n):
             if k >= 10000:
                 k = 0
 
-            stars_screen.fill(TYPE_OF_LEVEL[COLOR_INDEX])
+            # stars_screen.fill(TYPE_OF_LEVEL[COLOR_INDEX])
             board.render()
             score(sc)
 
@@ -267,7 +296,7 @@ def main(n):
         pygame.quit()
     elif n == 2:
         stars_screen = pygame.display.set_mode(size)
-        fon = pygame.transform.scale(load_image('store-fon.jpg'), SIZE)
+        fon = pygame.transform.scale(load_image('store.jpg'), SIZE)
         stars_screen.blit(fon, (0, 0))
 
         board = BoardShop(size[0], size[1], stars_screen)
@@ -276,12 +305,18 @@ def main(n):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        print(2)
                 if 696 <= pygame.mouse.get_pos()[0] <= 965 and 858 <= pygame.mouse.get_pos()[
                     1] <= 974 and event.type == pygame.MOUSEBUTTONDOWN:
                     start_screen(menu_screen)
+                if 218 <= pygame.mouse.get_pos()[0] <= 373 and 277 <= pygame.mouse.get_pos()[
+                    1] <= 369 and event.type == pygame.MOUSEBUTTONDOWN:
+                    SHTANI = SKINS[0]
+                if 496 <= pygame.mouse.get_pos()[0] <= 647 and 274 <= pygame.mouse.get_pos()[
+                    1] <= 362 and event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
+                if 411 <= pygame.mouse.get_pos()[0] <= 565 and 526 <= pygame.mouse.get_pos()[
+                    1] <= 618 and event.type == pygame.MOUSEBUTTONDOWN:
+                    pass
             pygame.display.flip()
         pygame.quit()
     elif n == 3:
